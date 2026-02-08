ee6a454054f9741dd00df8bbc411e172dcef40e8
adityaraj-devx <aadiiraj7780@gmail.com>
2026-02-08 17:11:38 +0530

    Implement add_setting function

--- User Configuration Manager/main.py
+++ User Configuration Manager/main.py
@@ -5,8 +5,8 @@ test_settings = {
     'notifications': 'allow',
 }
 def add_setting(settings, new_settings):
-    key = new_settings(0).lower
-    value = new_settings(1).lower
+    key = new_settings[0].lower()
+    value = new_settings[1].lower()
     if key in settings:
         return f"Setting {key} already exists! Cannot add a new setting with this name."
     else:
