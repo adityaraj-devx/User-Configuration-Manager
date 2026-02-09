fe68025cc63f8f438dba282a14dd12a5ffe30a35
adityaraj-devx <aadiiraj7780@gmail.com>
2026-02-09 21:10:08 +0530

    updated

--- User Configuration Manager/main.py
+++ User Configuration Manager/main.py
@@ -8,16 +8,16 @@ def add_setting(settings, new_settings):
     key = new_settings[0].lower()
     value = new_settings[1].lower()
     if key in settings:
-        return f"Setting {key} already exists! Cannot add a new setting with this name."
+        return f"Setting '{key}' already exists! Cannot add a new setting with this name."
     else:
         settings[key] = value
-        return f"Settings {key} added with value {value} successfully!"
+        return f"Settings '{key}' added with value '{value}' successfully!"
 
 def update_settings(settings, new_settings):
     key = new_settings[0].lower()
     value = new_settings[1].lower()
     if key in settings:
         settings[key] = value
-        return f"Settings {key} updated to {value} successfully!"
+        return f"Settings '{key}' updated to '{value}' successfully!"
     else:
-        return f"Setting {key} does not exist! Cannot update a non-existing setting."
+        return f"Setting '{key}' does not exist! Cannot update a non-existing setting."
